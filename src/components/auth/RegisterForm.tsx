7078443ef001b7d8aa87623ef233ae614c9e531a
WONILLISM <wonillism@gmail.com>
2023-07-16 18:22:20 +0900

    feat: add register form validation (#5)
    Birth value validation is not yet.

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -8,6 +8,9 @@ interface RegisterValues {
   birth: string;
   isCeleb: boolean;
 }
+
+type RegisterValue = keyof RegisterValues;
+
 const defaultRegisterValues: RegisterValues = {
   email: "",
   password: "",
@@ -20,8 +23,69 @@ const defaultRegisterValues: RegisterValues = {
 const RegisterForm = () => {
   const [values, setValues] = useState<RegisterValues>(defaultRegisterValues);
 
+  const [errors, setErrors] = useState<RegisterValues>(defaultRegisterValues);
+
+  const validation = (name: RegisterValue, value: string) => {
+    switch (name) {
+      case "email":
+        if (
+          !RegExp("^[a-zA-Z0-9+-_]+@[a-zA-Z0-9-_]+[.]+[a-zA-Z]+$").test(value)
+        ) {
+          setErrors({ ...errors, [name]: "올바르지 않은 이메일 형식입니다." });
+        } else {
+          setErrors({ ...errors, [name]: "" });
+        }
+        break;
+      case "password":
+        if (!RegExp("^.{8,}$").test(value)) {
+          setErrors({
+            ...errors,
+            [name]: "올바르지 않은 비밀번호 형식입니다.",
+          });
+        } else {
+          setErrors({ ...errors, [name]: "" });
+        }
+        break;
+      case "name":
+        if (!RegExp("^[가-힣a-zA-Z].{1,}$").test(value)) {
+          setErrors({
+            ...errors,
+            [name]: "올바르지 않은 이름 형식입니다.",
+          });
+        } else {
+          setErrors({ ...errors, [name]: "" });
+        }
+        break;
+      case "nickname":
+        if (!RegExp("^[a-zA-Z0-9+-_].{2,}$").test(value)) {
+          setErrors({
+            ...errors,
+            [name]: "올바르지 않은 별명 형식입니다.",
+          });
+        } else {
+          setErrors({ ...errors, [name]: "" });
+        }
+        break;
+      case "birth":
+        if (value === "") {
+          console.log("!!!");
+          setErrors({
+            ...errors,
+            [name]: "생일을 입력해주세요.",
+          });
+        } else {
+          setErrors({ ...errors, [name]: "" });
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleValuesChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+
+    validation(name as RegisterValue, value);
 
     setValues({ ...values, [name]: value });
   };
@@ -37,6 +101,7 @@ const RegisterForm = () => {
             value={values.email}
             onChange={handleValuesChange}
           />
+          <div>{!!values.email && errors.email}</div>
         </section>
         <section>
           <label>password</label>
@@ -46,15 +111,17 @@ const RegisterForm = () => {
             value={values.password}
             onChange={handleValuesChange}
           />
+          <div>{!!values.password && errors.password}</div>
         </section>
         <section>
           <label>name</label>
           <input
             name="name"
-            type="name"
+            type="text"
             value={values.name}
             onChange={handleValuesChange}
           />
+          <div>{!!values.name && errors.name}</div>
         </section>
         <section>
           <label>nickname</label>
@@ -64,6 +131,7 @@ const RegisterForm = () => {
             value={values.nickname}
             onChange={handleValuesChange}
           />
+          <div>{!!values.nickname && errors.nickname}</div>
         </section>
         <section>
           <label>birth</label>
@@ -73,6 +141,7 @@ const RegisterForm = () => {
             value={values.birth}
             onChange={handleValuesChange}
           />
+          <div>{!!!errors.birth && errors.birth}</div>
         </section>
         <button type="submit">register</button>
       </form>
