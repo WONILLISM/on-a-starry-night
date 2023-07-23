20b9499c8f3c9f053ddeb867b33843740be09e22
WONILLISM <wonillism@gmail.com>
2023-07-23 14:47:50 +0900

    feat: add login form validation

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -8,68 +8,42 @@ const LoginForm = () => {
     formState: { errors },
   } = useForm<LoginFormValues>({
     defaultValues: defaultLoginFormValues,
+    mode: "onChange",
   });
 
   const onSubmit: SubmitHandler<LoginFormValues> = (data) => console.log(data);
 
   return (
-    <article>
+    <section>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>email</label>
-        <input
-          {...register("email", {
-            required: true,
-            pattern: {
-              value: /^[a-zA-Z0-9+-_]+@[a-zA-Z0-9-_]+[.]+[a-zA-Z]+$/,
-              message: "invalid email type",
-            },
-          })}
-        />
-        {/* {errors.email?.type === "required" && <p>email is required</p>}
-        {errors.email?.type === "pattern" && <p>invalid email type</p>} */}
-        {errors.email?.message && <p>invalid email type</p>}
-
-        <label>password</label>
-        <input
-          {...register("password", {
-            required: true,
-            minLength: {
-              value: 8,
-              message: "Eeee",
-            },
-          })}
-        />
-        {errors.password?.type === "required" && <p>password is required</p>}
+        <div>
+          <input
+            {...register("email", {
+              required: { value: true, message: "Email is required." },
+              pattern: {
+                value: /^[a-zA-Z0-9+-_]+@[a-zA-Z0-9-_]+[.]+[a-zA-Z]+$/,
+                message: "Invalid Email type.",
+              },
+            })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <input
+            {...register("password", {
+              required: { value: true, message: "Password is required." },
+              minLength: {
+                value: 8,
+                message: "Password length is at least 8.",
+              },
+            })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
         <button type="submit">제출</button>
       </form>
-    </article>
+    </section>
   );
 };
 
 export default LoginForm;
-
-// const validation = (name: string, value: string) => {
-//   switch (name) {
-//     case "email":
-//       if (
-//         !RegExp("^[a-zA-Z0-9+-_]+@[a-zA-Z0-9-_]+[.]+[a-zA-Z]+$").test(value)
-//       ) {
-//         setErrors({ ...errors, [name]: "올바르지 않은 이메일 형식입니다." });
-//       } else {
-//         setErrors({ ...errors, [name]: "" });
-//       }
-//       break;
-//     case "password":
-//       if (!RegExp("^.{8,}$").test(value)) {
-//         setErrors({
-//           ...errors,
-//           [name]: "올바르지 않은 비밀번호 형식입니다.",
-//         });
-//       } else {
-//         setErrors({ ...errors, [name]: "" });
-//       }
-//       break;
-//     default:
-//       break;
-//   }
-// };
